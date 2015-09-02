a21f16f1a07f57126b252295ce4d90286bf719d0
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-09-02 11:55:48 +0500

    modal add

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -1,3 +1,13 @@
+jQuery(document).ready(function ($) {
+    $('a[data-click="modal"]').click(function () {
+        var modalName = $(this).attr('href');
+        $(modalName).addClass('active');
+        $('.modal-mask').addClass('active');
+    });
+    $('.modal-mask, .modal__close').click(function () {
+        $('.modal-mask, .modal').removeClass('active');
+    });
+});
 $(document).ready(function () {
     $('#slider').slick({
         speed: 1300,
